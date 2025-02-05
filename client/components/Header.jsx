947910784ef4eb419af3b1d789ce01f8784d582a
Zach White <zwhite@twinprimemedia.com>
2025-02-04 18:46:09 -0600

    header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -9,7 +9,7 @@ export const Header = () => {
             to="/"
             className="text-lg font-display font-bold tracking-wide hover:text-eggshell transition-colors"
           >
-            Luckenbach
+            Luckenbach 2
           </Link>
 
           <nav className="h-full">
